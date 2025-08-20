6bbc9574927c49fbaccc6a36661c27be35815b8c
SendriAska <thomas.boscaro@gmail.com>
2025-08-20 10:57:01 +0200

    Update categoryModel.php

--- App/Model/categoryModel.php
+++ App/Model/categoryModel.php
@@ -1,4 +1,9 @@
 <?php
+
+require_once __DIR__ . '../../Utils/Database.php';
+
+use App\Utils\Database;
+
 //ajouter une category (objet de connexion et le contenu de la catégorie)
 function addCategory(string $name)
 {
@@ -7,7 +12,7 @@ function addCategory(string $name)
         $request = "INSERT INTO category(`name`) VALUES (?)";
         //Ecrire toutes les étapes de la requêtes 
         //1 préparer la requête
-        $req = connectBDD()->prepare($request);
+        $req = Database::connectBDD()->prepare($request);
         //2 Bind les paramètres
         $req->bindParam(1, $name, PDO::PARAM_STR);
         //3 executer la requête
@@ -24,7 +29,7 @@ function getCategoryById(int $id)
     try {
         $request = "SELECT c.id_category, c.name FROM category AS c WHERE c.id_category = ?";
         //1 preparer la requête
-        $req = connectBDD()->prepare($request);
+        $req = Database::connectBDD()->prepare($request);
         //2 assigner le paramètre
         $req->bindParam(1, $id, PDO::PARAM_INT);
         //3 executer la requête
@@ -52,7 +57,7 @@ function getAllCategory():array
 {
     try {
         $request = "SELECT id_category, `name` FROM category ORDER BY id_category ASC";
-        $req = connectBDD()->prepare($request);
+        $req = Database::connectBDD()->prepare($request);
         $req->execute();
         $category =  $req->fetchAll(PDO::FETCH_ASSOC);
         return $category;
@@ -67,7 +72,7 @@ function getAllCategory():array
 function updateCategory(int $id, string $newName) {
     try {
         $request = "UPDATE category SET `name` = ? WHERE id_category = ?";
-        $req = connectBDD()->prepare($request);
+        $req = Database::connectBDD()->prepare($request);
         $req->bindParam(1, $newName, PDO::PARAM_STR);
         $req->bindParam(2, $id, PDO::PARAM_INT);
         $req->execute();
@@ -81,7 +86,7 @@ function updateCategory(int $id, string $newName) {
 function deleteCategory(int $id) {
     try {
         $request = "DELETE FROM category WHERE id_category = ?";
-        $req = connectBDD()->prepare($request);
+        $req = Database::connectBDD()->prepare($request);
         $req->bindParam(1, $id, PDO::PARAM_INT);
         $req->execute();
     } catch (Exception $e) {
